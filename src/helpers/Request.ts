17c2601c504f2fd7c3067aa543e3b57144fa35e1
AbdulazizSulaymon <djshahrick@gmail.com>
2025-04-10 19:51:33 +0500

    fix: baseurl

--- src/helpers/Request.ts
+++ src/helpers/Request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-export const BASE_URL = "";
+export const BASE_URL = "https://falaqnashr.uz/api/";
 
 export function Request<T = unknown>(
   url: string,
