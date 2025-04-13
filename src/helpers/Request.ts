50e85b0c4c16509d2a6a1136d80397c0b6c6359f
Dilshodbek <ziyodullayevdilshod7@gmail.com>
2025-04-13 17:34:10 +0500

    fix

--- src/helpers/Request.ts
+++ src/helpers/Request.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
-export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL = "https://falaqnashr.uz/api";
 
 export function Request<T = unknown>(
   url: string,
