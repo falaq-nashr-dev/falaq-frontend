ca17e98dc5368bf89bb08f78bc40448a70f46cff
Dilshodbek <ziyodullayevdilshod7@gmail.com>
2025-04-13 22:29:30 +0500

    fix modal size

--- src/pages/components/modals/description-modal.tsx
+++ src/pages/components/modals/description-modal.tsx
@@ -16,7 +16,7 @@ const DescriptionModal = ({
       <Drawer.Root onClose={handleClose} open={open} onOpenChange={handleClose}>
         <Drawer.Portal>
           <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="bg-gray-100 flex flex-col items-center rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none z-[200]">
+          <Drawer.Content className="bg-gray-100 flex flex-col items-center rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none z-[200] max-w-xl mx-auto">
             <Drawer.Title className="hidden"></Drawer.Title>
             <Drawer.Description className="hidden"></Drawer.Description>
 
--- src/pages/components/modals/review-modal.tsx
+++ src/pages/components/modals/review-modal.tsx
@@ -50,7 +50,7 @@ const ReviewModal = ({
       <Drawer.Root onClose={handleClose} open={open} onOpenChange={handleClose}>
         <Drawer.Portal>
           <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-          <Drawer.Content className="bg-gray-100 flex flex-col items-center rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none z-[200]">
+          <Drawer.Content className="bg-gray-100 flex flex-col items-center rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none z-[200] max-w-xl mx-auto">
             <Drawer.Title className="hidden"></Drawer.Title>
             <Drawer.Description className="hidden"></Drawer.Description>
 
@@ -66,7 +66,7 @@ const ReviewModal = ({
                 aria-hidden
                 className="mx-auto w-12 h-1.5 rounded-full bg-gray-300 mb-6"
               />
-              <div className="h-[50vh] overflow-y-auto">
+              <div className="h-[80vh] overflow-y-auto">
                 {/* 5 star for review */}
                 <StarRating onRatingChange={handleRatingChange} />
                 <div>
