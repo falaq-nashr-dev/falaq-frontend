ecf42907afe6afa08d2fb141bb436182fbae703b
Dilshodbek <ziyodullayevdilshod7@gmail.com>
2025-10-02 15:37:20 +0500

    fix bug

--- src/helpers/Request.ts
+++ src/helpers/Request.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 // export const BASE_URL = "http://localhost:8080/api";
-export const BASE_URL = "https://198.163.206.88/api";
-// export const BASE_URL = "https://falaqnashr.uz/api";
+export const BASE_URL = "https://falaqnashr.uz/api";
 
 export function Request<T = unknown>(
   url: string,
