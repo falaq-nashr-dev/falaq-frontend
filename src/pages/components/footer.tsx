40a68b18c1e093d87438ea7afc8960c524d1c636
falaq-nashr-dev <falaqnashr.app@gmail.com>
2025-10-10 07:41:55 +0500

    Update footer.tsx
    footer changes

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
                 Bizga obuna bo&apos;ling
               </h4>
               <div className="flex space-x-3">
-                <Link
+                {/* <Link
                   to="/"
                   className="w-8 h-8 bg-blue-500 rounded flex items-center justify-center text-white hover:bg-blue-600 transition-colors"
                 >
@@ -42,8 +42,8 @@ const Footer = () => {
                   >
                     <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
                   </svg>
-                </Link>
-                <Link
+                </Link> */}
+                {/* <Link
                   to="/"
                   className="w-8 h-8 bg-red-500 rounded flex items-center justify-center text-white hover:bg-red-600 transition-colors"
                 >
@@ -54,8 +54,8 @@ const Footer = () => {
                   >
                     <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                   </svg>
-                </Link>
-                <Link
+                </Link> */}
+                {/* <Link
                   to="/"
                   className="w-8 h-8 bg-pink-500 rounded flex items-center justify-center text-white hover:bg-pink-600 transition-colors"
                 >
@@ -66,7 +66,7 @@ const Footer = () => {
                   >
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                   </svg>
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
@@ -99,22 +99,6 @@ const Footer = () => {
                   Barcha kitoblar
                 </Link>
               </li>
-              <li>
-                <Link
-                  to="#"
-                  className="text-gray-600 hover:text-blue-600 transition-colors"
-                >
-                  Eng ko&apos;p o&apos;qilgan kitoblar
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="text-gray-600 hover:text-blue-600 transition-colors"
-                >
-                  Bestseller
-                </Link>
-              </li>
             </ul>
           </div>
 
@@ -122,14 +106,6 @@ const Footer = () => {
           <div className="space-y-4">
             <h4 className="font-semibold text-gray-800">Kompaniya haqida</h4>
             <ul className="space-y-3">
-              <li>
-                <Link
-                  to="#"
-                  className="text-gray-600 hover:text-blue-600 transition-colors"
-                >
-                  Biz haqimizda
-                </Link>
-              </li>
               <li>
                 <Link
                   to="#"
@@ -145,23 +121,7 @@ const Footer = () => {
                 >
                   Terms & Conditions
                 </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="text-gray-600 hover:text-blue-600 transition-colors"
-                >
-                  To&apos;lov va yetkazib berish
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="#"
-                  className="text-gray-600 hover:text-blue-600 transition-colors"
-                >
-                  Qaytarish
-                </Link>
-              </li>
+              </li> 
             </ul>
           </div>
 
@@ -188,21 +148,20 @@ const Footer = () => {
                 <MapPin className="w-5 h-5 text-blue-500 mt-1 flex-shrink-0" />
                 <div className="text-gray-600">
                   <p>O'zbekiston, Toshkent shahri</p>
-                  <p>CA 94107, United States</p>
                 </div>
               </div>
 
               {/* Phone */}
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-blue-500 flex-shrink-0" />
-                <span className="text-gray-600">+998 88 111 47 47</span>
+                <span className="text-gray-600">+998 77 191 91 31</span>
               </div>
 
               {/* Email */}
-              <div className="flex items-center space-x-3">
+              {/* <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-blue-500 flex-shrink-0" />
                 <span className="text-gray-600">support@falaqnashr</span>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
